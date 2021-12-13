50e4d5745da94efa8786ac711aac9a97931c92dd
Mwanja Joel <joelsilverworks@gmail.com>
2021-12-13 22:23:56 +0300

    Lights! Code! Flutter! 🚀

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    // generate a random word pair, randomly
+    final wordPair = WordPair.random();
+
+    return MaterialApp(
+      theme: ThemeData(
+          primaryColor: Colors.purple[900],
+        ),
+      home: RandomWords(),
+      
+      );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override 
+  RandomWordsState createState() => RandomWordsState();
+
+}
+
+class RandomWordsState extends State<RandomWords> {
+
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return const Divider();
+
+        final index = item ~/ 2;
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_randomWordPairs[index]);
+      },
+      
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator')),
+      body: _buildList(),
+    );
+  }
+}
+
